cab2c5a3af070e463b50e2df6ebd43493af07875
jordanobl <jordan.williams@codemith.io>
2024-10-27 21:10:56 -0400

    logging

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,25 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/jordanOBL/sleepBubble/internal/server"
 )
-
 func main() {
-	//Create New Server
-	server, app, logger := server.NewServer()
+    // Create New Server
+    server, app, logger := server.NewServer()
 
-	//Load Db of connected ExpotTokens 
-	//into servers Client List
-	err := app.LoadSubscribedTokens("sleepbubble.csv")
-	if err != nil {
-		fmt.Printf("error loading subscribed Tokens: %v", err)
-	}
+    // Load DB of connected Expo tokens into the server's client list
+    err := app.LoadSubscribedTokens("sleepbubble.csv")
+    if err != nil {
+        logger.Error("error loading subscribed tokens", "err", err.Error())
+    }
 
-	// Start the server
-	err = server.ListenAndServe()
-	logger.Error(err.Error())
-	os.Exit(1)
-	select {}
+    // Start the server
+    logger.Info("Starting server...")
+    if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+        logger.Error("Server error", "err", err.Error())
+        os.Exit(1)
+    }
 }
+
